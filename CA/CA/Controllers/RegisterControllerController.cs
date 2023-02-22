84143930eafc6184da3273907f0d774bb01d0017
EthanFeng-coder <s903586818@gmail.com>
2023-02-22 12:01:32 +1100

    CapitalAnalyst Web BACKEND

---
+++ CA/CA/Controllers/RegisterControllerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CA.Db;
+using CA.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CA.Controllers
+{
+    [Route("api/[controller]")]
+    [Controller]
+    public class RegisterController : ControllerBase
+    {
+        private readonly AppDbContext _dbContext;
+
+        public RegisterController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] User user)
+        {
+            // 添加用户到数据库
+            _dbContext.Users.Add(user);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(new { message = "Registration successful" });
+        }
+    }
+}
+
---
+++ CA/CA/Migrations/20230208002854_InitialCreate1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+namespace CA.Migrations
+{
+    public partial class InitialCreate1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Texts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Author = table.Column<string>(type: "text", nullable: false),
+                    Category = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Texts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    UserName = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Texts");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
